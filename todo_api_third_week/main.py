c2a68f27d6af918bbbf7f3ede839d9ec3b265178
Osmolovsky K. <kirillosm09@gmail.com>
2025-12-12 12:46:30 +1000

    3 week: init project

---
+++ todo_api_third_week/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from database import Query 
+
+app = FastAPI()
+db = Query()
+
+@app.get('/api/v1/{user_id}/{note_id}')
+async def api_read(user_id: int, note_id: int):
+    try:
+        note_date, note_text = await db.get_note(user_id, note_id)
+        return {
+                'user_id': user_id,
+                'note_id': note_id,
+                'note_date': note_date,
+                'note_text': note_text,
+                }
+
+    except Exception as e:
+        return {'error_message': e}
+
+
+@app.delete('/api/v1/{user_id}/{note_id}')
+async def api_delete(user_id: int, note_id: int):
+    try:
+        succeed = await db.delete_note(user_id, note_id)
+        return {'status': succeed}
+    except Exception as e:
+        return {'error_message': e}
+
+
+@app.post('/api/v1/{user_id}/create')
+async def api_create(user_id: int, note_text: str, note_date: str):
+    try:
+        await db.ensure_user(user_id)
+        new_note_id = await db.new_note(user_id, note_text, note_date)
+        return {
+                'user_id': user_id,
+                'note_id': new_note_id,
+                }
+    except Exception as e:
+        return {'error_message': e}
